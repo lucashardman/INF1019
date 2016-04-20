2308af08c744e6e868aa2fa09a9dbb7906a5c4fd
Lucas Hardman <lucashardman@localhost.localdomain>
2016-04-19 22:53:42 -0300

    Executa os programas e envia sinal para entrar em espera

--- Trabalho1/escalonadores.c
+++ Trabalho1/escalonadores.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "escalonadores.h"
 
@@ -33,22 +34,25 @@ void escalonamentoRoundRobin(int quantidadeProgramas, ProgramaRoundRobin *progra
 	for(loop1=0;loop1<quantidadeProgramas;loop1++){
 		programas[loop1]->terminado = false;
 	}
-	/*TESTAR ESTA PARTE DO CODIGO*/
+	/* Executa os programas e envia o sinal para entrarem em espera */
 	for(loop1=0;loop1<quantidadeProgramas;loop1++){
-		
-		//printf("%s\n", programas[loop1]->nome);
 
 		pid_auxiliar = fork();
 
 		if(pid_auxiliar!=0){
+
 			pid[loop1] = pid_auxiliar;
 			kill(pid_auxiliar, SIGSTOP);
+			printf("Estou aqui\n");
 		}
 		else{
+			printf("TESTE: %s\n",programas[loop1]->nome);
 			sleep(1);
 			execve(programas[loop1]->nome,NULL,NULL);
+			
 		}
 	}
+	/* FIM: Executa os programas e envia o sinal para entrarem em espera */
 
 }
 
