fd405110e957f106f481d5d84837a748e839b6bd
Lucas Hardman <lucashardman@localhost.localdomain>
2016-04-21 13:28:07 -0300

    testaProgramasFinalizados e iniciarProgramas funcionando

--- Trabalho1/escalonadores.c
+++ Trabalho1/escalonadores.c
@@ -6,9 +6,14 @@
 
 /* Variaveis globais */
 static int metodoEscalonamento;
+
+static ProgramaPrioridade *progPrioridade[maximo_programas]; //Variavel responsavel pelo controle dos programas pela politica por prioridade
+static ProgramaRoundRobin *progRoundRobin[maximo_programas]; //Variavel responsavel pelo controle dos programas pela politica por round robin
+static ProgramaLoteria *progLoteria[maximo_programas]; //Variavel responsavel pelo controle dos programas pela politica por loteria
+
 /* Funcoes estaticas */
-static void iniciarProgramas(int quantidadeProgramas, char nome[15][maximo_programas], int *pid);
-static bool testaProgramasFinalizados(int quantidadeProgramas, void *programas[maximo_programas]);
+static void iniciarProgramas(int quantidadeProgramas, int *pid);
+static bool testaProgramasFinalizados(int quantidadeProgramas);
 
 /* Funcoes de escalonamento */
 
@@ -34,30 +39,30 @@ void escalonamentoRoundRobin(int quantidadeProgramas, ProgramaRoundRobin *progra
 	int loop1 = 0, loop2; //Variaveis auxiliares para loop
 	int pid[maximo_programas], pid_auxiliar = 0; //Variaveis responsaveis por guardar os pid dos processos
 
-	char stringAux[15][maximo_programas]; //Variavel auxiliar para copias de strings
-
 	metodoEscalonamento = 2;
-
-	/* Inicializar o campo "terminado" com false */
-	for(loop1=0;loop1<quantidadeProgramas;loop1++){
-		programas[loop1]->terminado = false;
-	}
 
 	/* Executa os programas e envia o sinal para entrarem em espera */
 	for(loop1=0;loop1<quantidadeProgramas;loop1++){
-		strcpy(stringAux[loop1], programas[loop1]->nome);
+		progRoundRobin[loop1] = programas[loop1];
 	}
-	iniciarProgramas(quantidadeProgramas, stringAux, pid);
+	iniciarProgramas(quantidadeProgramas, pid);
 	/* Fim: Executa os programas e envia o sinal para entrarem em espera */
 
+	/* Inicializar o campo "terminado" com false */
 	for(loop1=0;loop1<quantidadeProgramas;loop1++){
-		printf("O programa: %s de pid %d foi iniciado.\n", programas[loop1]->nome, pid[loop1]);
+		progRoundRobin[loop1]->terminado = false;
+	}
+
+	for(loop1=0;loop1<quantidadeProgramas;loop1++){
+		printf("O programa: %s de pid %d foi iniciado.\n", progRoundRobin[loop1]->nome, pid[loop1]);
 	}
 
 	/* Inicio do algoritmo de Round-Robin */
 	while(1){
-		if(testaProgramasFinalizados(quantidadeProgramas, (void*)programas) == true)
+		if(testaProgramasFinalizados(quantidadeProgramas) == true) // Testa se todos os programas ja foram finalizados
 			return; //Finaliza a funcao de Round-Robin
+
+		
 	}
 	/* Fim do algoritmo de Round-Robin*/
 }
@@ -81,10 +86,9 @@ void escalonamentoLoteria(int quantidadeProgramas, ProgramaLoteria *programas[ma
  * utilizando execve e enviar um sinal para eles entrarem em espera SIGSTOP *
  * Parametros:																*
  * quantidadeProgramas - quantidade de programas sendo gerenciados			*
- * nome - vetor com o nome dos programas que estao sendo gerenciados		*
  * pid - vetor responsavel por retornar o pid dos programas                 *
  ****************************************************************************/
-static void iniciarProgramas(int quantidadeProgramas, char nome[15][maximo_programas], int *pid){
+static void iniciarProgramas(int quantidadeProgramas, int *pid){
 
 	int loop = 0;
 	int pid_auxiliar = 0; //Variaveis responsaveis por guardar os pid dos processos
@@ -98,13 +102,22 @@ static void iniciarProgramas(int quantidadeProgramas, char nome[15][maximo_progr
 
 			pid[loop] = pid_auxiliar;
 			kill(pid_auxiliar, SIGSTOP);
-			printf("Estou aqui\n");
 		}
 		else{
-			printf("Programa %s iniciado.\n",nome[loop]); //Para este print aparecer, comentar o kill
 			sleep(3); //Programas iniciados com uma diferenca de 3 segundos entre cada
-			execve(nome[loop],NULL,NULL);
-			
+			if(metodoEscalonamento == 1){
+				execve(progPrioridade[loop]->nome,NULL,NULL);
+			}
+			else if(metodoEscalonamento == 2){
+				execve(progRoundRobin[loop]->nome,NULL,NULL);
+			}
+			else if(metodoEscalonamento == 3){
+				execve(progLoteria[loop]->nome,NULL,NULL);
+			}
+			else{
+				printf("Problema com a identificacao do metodo de escalonamento na funcao iniciarProgramas\nPor favor, reinicie o programa.\n");
+				exit(1);
+			}
 		}
 	}
 	/* FIM: Executa os programas e envia o sinal para entrarem em espera */
@@ -116,48 +129,55 @@ static void iniciarProgramas(int quantidadeProgramas, char nome[15][maximo_progr
  * finalizados.                                                             *
  * Parametros:																*
  * quantidadeProgramas - quantidade de programas sendo gerenciados			*
- * programas - vetor com os programas que estao sendo gerenciados	    	*
  * Condicoes de retorno:                                                    *
  * true - todos os programas ja foram finalizados                           *
  * false - ainda faltam programas a serem finalizados                       *
  ****************************************************************************/
-static bool testaProgramasFinalizados(int quantidadeProgramas, void *programas[maximo_programas]){
+static bool testaProgramasFinalizados(int quantidadeProgramas){
 
 	int loop = 0;
 	int contadorTerminados = 0;
 
-	/************************
-	 *Bloco de teste:       */
-	 printf("Teste de alcance para funcao testaProgramasFinalizados");
+	/**********************************************************************
+	//Bloco de teste:       
+	// printf("Teste de alcance para funcao testaProgramasFinalizados\n");
+	//
+	// printf("Lista de programas:\n");
+	// for(loop=0;loop<quantidadeProgramas;loop++)
+	// 	printf("%s\n", progRoundRobin[loop]->nome);
+	/**********************************************************************/
 
-	 printf("Lista de programas:\n");
-	 for(loop=0;loop<quantidadeProgramas;loop++)
-	 	printf("%s\n", ((ProgramaRoundRobin)programas[loop])->nome);
-	/************************/
+	progRoundRobin[0]->terminado = true;
 
-/*
 	for(loop=0;loop<quantidadeProgramas;loop++){
 		
 		if(metodoEscalonamento == 1){
-			if((ProgramaPrioridade)programas[loop]->terminado == false){
+			if(progPrioridade[loop]->terminado == false){
 				contadorTerminados++;
 			}
 		}
 		if(metodoEscalonamento == 2){
-			if((ProgramaRoundRobin)programas[loop]->terminado == false){
+			if(progRoundRobin[loop]->terminado == false){
 				contadorTerminados++;
 			}
 		}
 		if(metodoEscalonamento == 3){
-			if((ProgramaLoteria)programas[loop]->terminado == false){
+			if(progLoteria[loop]->terminado == false){
 				contadorTerminados++;
 			}
 		}
 	}
-*/
+
 	if(contadorTerminados == quantidadeProgramas){
-		return false;
+		//Teste	printf("Todos terminados\n");
+		return true; // Retorna aviso de que todos os programas foram finalizados
 	}
 
-	return true;
+	/* TESTE
+	for(loop=0;loop<quantidadeProgramas;loop++)
+		if(progRoundRobin[loop]->terminado == false)
+			printf("Falta %s\n", progRoundRobin[loop]->nome);
+	*/
+
+	return false; //Retorna aviso de que ainda faltam programas a serem finalizados
 }
