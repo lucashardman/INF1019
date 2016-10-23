a03a31533daee7972610715ec7145769acf20135
Lucas Hardman <lucashardman@localhost.localdomain>
2016-10-23 16:13:56 -0200

    prioridades funcionando corretamente.

--- Escalonadores_2016.2/prioridades.c
+++ Escalonadores_2016.2/prioridades.c
@@ -33,14 +33,15 @@ typedef struct programa
 Programa *lista;
 int qtd=0;
 int terminado=0;
+bool reinicia=false;
 
 void carrega_programa(int signo);
 void encerra(int signo);
 static int *ordemPrioridade(int quantidadeProgramas, int *ordem);
 
 int main (int argc, char *argv[]) { 
-	int pid = 0, pidPai=0, status;
-	int loop = 0, i=0, j=0;
+	int pid=0, pidPai=0, status;
+	int loop=0, i=0, j=0, contPrioridade=0;
 	int ordem[MAX_PROG];
 
 	time_t s; // o segundo corrente em cada loop
@@ -59,73 +60,92 @@ int main (int argc, char *argv[]) {
 		}
 	}
 	//FIM: se fifo existe, abre fifo para leitura
+
 	
-	
-	printf("O PID EH %s\n", argv[1]);
 
-	pidPai = atoi(argv[1]);
+	for(i=0;i<=7;i++){ //Loop para cada prioridade
 
-	printf("O PID INT EH %d\n", pidPai);	
+		j=0;
+		contPrioridade = 0;
 
-	while(1){
-
-		for(loop=0;loop<qtd;loop++){ // Este loop eh apenas para garantir que os programas estao todos em estado de espera antes do escalonamento comecar.
-			kill(lista[loop].pid, SIGSTOP);
-		}
-
-		sleep(3);
-		
+		sleep(3); //Espera 3 segundos para dar tempo de carregar alguns programas.
+		//OBS: Se os programas com prioridades maiores forem carregador depois 
+				
 		fflush(stdout);	
-		/* O vetor ordem armazena a ordem de programas que estao sendo escalonados. Como a funcao ordemPrioridade esta sendo chamada 
-		 * em todo o loop, o vetor ordem esta sendo atualizado em tempo real. Ou seja, quando um novo programa entra para o escalonamento,
-		 * ele automaticamente eh colocado para executar na sua ordem certa.
-		 * OBS: Apos um programa ser carregado, ele soh entra para o escalonamento no proximo loop. Por exemplo, temos 3 programas: x de prioridade 1
-		 * y de prioridade 2 e z de prioridade 3. Se x estiver em execucao, z em estado de espera e y acabou de ser carregado, z vai ser o proximo
-		 * programa a ser escalonado (mas no proximo loop, y sera escalonado entre x e z). */
-		ordemPrioridade(qtd, ordem);
 		
-		//inicio = time(NULL);
-
-		i=0;
-		while(i<qtd){
-
-			//s = (time(NULL) - inicio)%60;
-
-			for(j=0;j<qtd;j++){
-				/* Compara a prioridade de lista[j] com a ordem[i]. Se for a mesma, entao esta na vez do programa
-				 * de lista[j] ser escalonado. */
-				if(lista[j].prioridade == ordem[i] && lista[j].terminado == false){
-
-					printf("%s de prioridade %d vai recomecar.\n", lista[j].nome, ordem[i]);
-
-					if(waitpid(lista[j].pid, &status, WNOHANG) != 0){ //Waitpid != 0, se o processo com pid passado como argumento estiver terminado
-						printf("--> Programa %s terminado\n", lista[j].nome);
-						lista[j].terminado = true;
-					}
-					for(loop=0;loop<qtd;loop++){//Testa se todos os programas terminaram.
-						if(lista[loop].terminado == true){
-							terminado++;
-						}
-					}
-					if(terminado == qtd){//Se todos programas terminaram, termina.
-						printf("--> Todos os programas terminaram.\n");
-						kill(pidPai, SIGKILL); //Mata o processo pai.
-						return 0;
-					}
-					terminado = 0;
-
-					fflush(stdout);
-					sleep(1);
-					kill(lista[j].pid, SIGCONT); // O programa eh escalonado. Entra em estado de execucao.
-					fflush(stdout);
-					sleep(3); // O programa executa por 3 segundos.
-					kill(lista[j].pid, SIGSTOP); // O programa passa a vez. Entra em estado de espera.
-				}
+		for(j=0;j<qtd;j++){
+			if(lista[j].prioridade == i){
+				contPrioridade++;
 			}
-			i++;
 		}
-	}	
-	return 0; 
+		if(contPrioridade == 0){
+			if(i==7){
+				i=0;
+			}
+			continue;
+		}
+		else{
+			printf("Escalonando %d programas de prioridade %d\n", contPrioridade, i);
+		}
+
+		j=0;
+		while(contPrioridade > 0){ //Loop importante para ficar esperando novos programas
+			
+			for(loop=0;loop<qtd;loop++){ // Este loop eh apenas para garantir que os programas estao todos em estado de espera antes do escalonamento comecar.
+				kill(lista[loop].pid, SIGSTOP);
+			}
+
+			fflush(stdout);	
+
+			if(lista[j].prioridade == i && lista[j].terminado == false){
+					
+				printf("%s de prioridade %d vai recomecar.\n", lista[j].nome, i);
+
+				if(waitpid(lista[j].pid, &status, WNOHANG) != 0){ //Waitpid != 0, se o processo com pid passado como argumento estiver terminado
+					printf("--> Programa %s terminado\n", lista[j].nome);
+					lista[j].terminado = true;
+					lista[j].prioridade = 0; //Isto faz com que a prioridade deste programa n~ao se repita.
+					contPrioridade--;
+				}
+				for(loop=0;loop<qtd;loop++){//Testa se todos os programas terminaram.
+					if(lista[loop].terminado == true){
+						terminado++;
+					}
+				}
+				if(terminado == qtd){//Se todos programas terminaram, termina.
+					printf("--> Todos os programas terminaram.\n");
+					kill(pidPai, SIGUSR1);
+					return 0;
+				}
+				terminado = 0;
+
+				fflush(stdout);
+				sleep(1);
+				kill(lista[j].pid, SIGCONT); // O programa eh escalonado. Entra em estado de execucao.
+				fflush(stdout);
+				sleep(3); // O programa executa por 3 segundos.
+				kill(lista[j].pid, SIGSTOP); // O programa passa a vez. Entra em estado de espera.
+			}
+			j++;
+			if(j==qtd){
+				j=0;
+			}
+		}
+
+		//O flag reinicia serve pra recomecar a lista de prioridades caso um programa com prioridade mais alta
+		//seja carregado depois que um programa de priridade mais baixa ja comecou.
+		if(reinicia == true){
+			
+			i=0;
+			reinicia=false;
+			continue;
+		}
+
+		//Serve para deixar o loop infinito, pois sem isso nao eh possivel ESPERAR carregar todos os programas
+		if(i==7){
+			i=0;
+		}
+	}
 } 
 
 /****************************************************************************
@@ -175,45 +195,11 @@ void carrega_programa(int signo){
 	printf("Programa %s de PID %d e prioridade %d carregado.\n", lista[qtd].nome, lista[qtd].pid, lista[qtd].prioridade);
 	fflush(stdout);
 	qtd++;
+	reinicia = true;
 }
 
 //função chamada para fechar a fifo
 void encerra(int signo){	
 	close(fpFIFO); // O vetor ordem armazena a ordem de programas que estao sendo escalonados. 
 	return;
-}
-
-/****************************************************************************
- * Nome: ordemPrioridade                                                    *
- * Descricao: serve para informar a ordem de prioridade dos programas.      *
- * Utiliza um algoritimo de ordenacao por bolha (bubble sort) para ordenar  *
- * as prioridades dos programas.                                            *
- * Parametros:                                                              *
- * quantidadeProgramas - quantidade de programas sendo escalonados          *
- * Retorno:                                                                 *
- * ordem - vetor com a ordem de execucao dos programas. Cada posicao do     *
- * corresponde a prioridade de um programa                                  *
- ****************************************************************************/
-static int *ordemPrioridade(int quantidadeProgramas, int *ordem){
-	
-	int loop1 = 0, loop2 = 0;
-	//int *ordem;
-	int auxiliar = 100;
-
-	//ordem = (int*) malloc (sizeof(int)*quantidadeProgramas);
-
-	for(loop1=0;loop1<quantidadeProgramas;loop1++){
-		ordem[loop1] = lista[loop1].prioridade;
-	}
-
-	for(loop1=quantidadeProgramas-1;loop1>=1;loop1--){
-		for(loop2=0;loop2<loop1;loop2++){
-			if(ordem[loop2]>ordem[loop2+1]){
-				auxiliar = ordem[loop2];
-				ordem[loop2] = ordem[loop2+1];
-				ordem[loop2+1] = auxiliar;
-			}
-		}
-	}
-	return ordem;
 }
